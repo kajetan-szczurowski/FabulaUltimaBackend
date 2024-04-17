3ca51cc3b43e0de5021d2e40c7ad3821a6d6afdf
Kajetan Szczurowski <72921975+kajetan-szczurowski@users.noreply.github.com>
2024-04-17 18:13:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,11 +60,12 @@ const cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"]
-  }
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["http://localhost:5173"]
+//   }
+// });
+const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
 const cloudinary = require('cloudinary').v2;
